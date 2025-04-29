995a2243f1df185596fb7b89b82fcff72c48a1f3
mnivedcodinoverse <nived.marumamula@codinoverse.com>
2025-04-29 15:45:22 +0530

    created and added docker setups and kubernets setup

--- src/Superuser/usermodal.jsx
+++ src/Superuser/usermodal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./UserModal.css";
+import "./usermodal.css";
 import { useNavigate } from "react-router-dom"; // if using react-router for redirection
 
 const UserModal = ({ isOpen, onClose, onCreateUser }) => {
